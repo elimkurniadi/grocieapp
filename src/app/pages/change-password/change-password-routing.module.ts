b7c1540a679bfcad08072aaf2e13f5e0e9967a1b
Indra Abdulah <indra.abdullah99@gmail.com>
2020-12-22 05:16:46 +0700

    implement page request & verify otp

---
+++ src/app/pages/change-password/change-password-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RequestOtpComponent } from './request-otp/request-otp.component';
+import { VerifyOtpComponent } from './verify-otp/verify-otp.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'request-otp/forgot-password',
+    pathMatch: 'full',
+  },
+  {
+    path: 'request-otp/:type',
+    component: RequestOtpComponent
+  },
+  {
+    path: 'verify-otp',
+    component: VerifyOtpComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ChangePasswordPageRoutingModule {}
---
+++ src/app/pages/change-password/change-password.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ChangePasswordPageRoutingModule } from './change-password-routing.module';
+
+import { RequestOtpComponent } from './request-otp/request-otp.component';
+import { VerifyOtpComponent } from './verify-otp/verify-otp.component';
+import { SharedModule } from '@shared/shared.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    ChangePasswordPageRoutingModule,
+    SharedModule
+  ],
+  declarations: [
+    RequestOtpComponent,
+    VerifyOtpComponent
+  ]
+})
+export class ChangePasswordPageModule {}
