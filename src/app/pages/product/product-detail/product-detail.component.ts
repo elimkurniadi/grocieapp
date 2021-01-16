4d87c088e5cfe7c73bc3c3bf6f70f15f963d9f95
Roby Ibrahim Renata <robyrenata@gmail.com>
2021-01-16 17:01:54 +0700

    product detail's CTA show toast, update i18n

--- src/app/pages/product/product-detail/product-detail.component.ts
+++ src/app/pages/product/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@shared/pipes/translate/translate.service';
+import { ToastService } from '@shared/services/toast.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  constructor(private translateSrv: TranslateService, private toastSrv: ToastService) {}
+  isFavorite = false;
 
   ngOnInit() {}
 
+  ctaFavorite() {
+    this.isFavorite = !this.isFavorite;
+    const message = this.isFavorite
+      ? this.translateSrv.get('SUCCESS_ADD_FAVORITE')
+      : this.translateSrv.get('SUCCESS_REMOVE_FAVORITE');
+    this.toastSrv.show(message);
+  }
+
+  addToCart() {
+    const message = this.translateSrv.get('SUCCESS_ADD_TO_CART');
+    this.toastSrv.show(message);
+  }
 }
