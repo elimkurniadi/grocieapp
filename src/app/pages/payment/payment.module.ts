d1b7cc03fc5070854f0b3fc269114a11015a4098
Indra Abdulah <indra.abdullah99@gmail.com>
2021-02-19 13:16:26 +0700

    implement page cod

--- src/app/pages/payment/payment.module.ts
+++ src/app/pages/payment/payment.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,9 +9,10 @@ import { PaymentPageRoutingModule } from './payment-routing.module';
 import { PaymentPage } from './payment.page';
 import { PaymentListComponent } from './payment-list/payment-list.component';
 import { SharedModule } from '@shared/shared.module';
+import { PaymentCodComponent } from './payment-cod/payment-cod.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, PaymentPageRoutingModule, SharedModule],
-  declarations: [PaymentPage, PaymentListComponent],
+  imports: [CommonModule, FormsModule, IonicModule, PaymentPageRoutingModule, SharedModule, ReactiveFormsModule],
+  declarations: [PaymentPage, PaymentListComponent, PaymentCodComponent],
 })
 export class PaymentPageModule {}
