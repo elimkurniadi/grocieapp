896e3b96531a4ab04c76497c0cf9f1556d1eb109
Indra Abdulah <indra.abdullah99@gmail.com>
2021-02-19 18:31:58 +0700

    integrate payment transaction midtrans

--- src/app/pages/payment/payment-list/payment-list.component.ts
+++ src/app/pages/payment/payment-list/payment-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BrowserService } from '@shared/services/browser.service';
+import { AddressService, TransactionService } from '@shared/services/modules';
 
 @Component({
   selector: 'app-payment-list',
@@ -8,14 +10,43 @@ import { Router } from '@angular/router';
 })
 export class PaymentListComponent implements OnInit {
   paymentMethod: string;
+  address: any;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private transactionSrv: TransactionService,
+    private addressSrv: AddressService,
+    private browserSrv: BrowserService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAddress();
+  }
 
   next() {
-    if (this.paymentMethod === 'cod') {
+    if (this.paymentMethod === 'online_bank') {
+      this.payOrder();
+    } else if (this.paymentMethod === 'cod') {
       this.router.navigate(['/payment', 'cod']);
     }
+  }
+
+  getAddress() {
+    this.addressSrv.getAddress().then((res) => {
+      this.address = res[0];
+    });
+  }
+
+  payOrder() {
+    const body = {
+      is_now: true,
+      address_id: this.address.address_id,
+      shipping_id: 1,
+      payment_method_id: 1,
+    };
+
+    this.transactionSrv.add(body).then((res) => {
+      this.browserSrv.openBrowser({ url: res.response });
+    });
   }
 }
